66151d6ea6c47dd1a2c4f8a8d83631f8e347c4d0
Aobakwe Segone <Aobakwe.Segone@gijima.com>
2019-02-05 20:21:01 +0200

    Initial Commit

---
+++ WeatherForecast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherForecast
+//
+//  Created by Aobakwe Segone on 2019/02/05.
+//  Copyright © 2019 aobakwesegone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
