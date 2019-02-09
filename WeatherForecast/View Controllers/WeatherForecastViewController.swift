e506316fbd6255522349cb16054672439162fca8
Aobakwe Segone <Aobakwe.Segone@gijima.com>
2019-02-09 07:05:47 +0200

    adding days for a five day weather forecast and wiring the table view forecast section

--- WeatherForecast/View Controllers/WeatherForecastViewController.swift
+++ WeatherForecast/View Controllers/WeatherForecastViewController.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 
+enum DaysOfTheWeek: Int {
+    case Sunday = 1, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
+}
+
 class WeatherForecastViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     var store: WeatherStore!
     var cellViewModels = [ForecastWeatherCell]()
@@ -22,66 +26,100 @@ class WeatherForecastViewController: UIViewController, UITableViewDataSource, UI
     @IBOutlet weak var currentTempLabel: UILabel!
     
     @IBOutlet weak var forecastTableView: UITableView!
+    
+    // MARK:- view life cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         NotificationCenter.default.addObserver(self, selector: #selector(receivedForecastWeather), name: NSNotification.Name(rawValue: "ForecastWeather Available"), object: nil)
-
-//        store.fetchfiveDayWeatherForecast(with: "-25.92", and: "28.14") { (weatherResult) in
-//            switch weatherResult {
-//            case let .Success(weatherResult):
-//                //print("successfully decoded:\n\(weatherResult.list)")
-////                self.cellViewModels = weather.list.map {
-////                    AESWeatherCell(weekDay: $0.dt_txt, maximimTemperature: $0.main.maximumTemperature, iconURL: URL(string: "https://openweathermap.org/img/w/\($0.weather[0].icon).png")!)
-////                }
-////                OperationQueue.main.addOperation {
-////                    self.forecastTableView.reloadData()
-////                }
-//
-//                                self.store.fetchWeatherIconForWeather(weather: weather.first!, completion: { (imageResult) in
-//                                    switch imageResult {
-//                                    case let .Success(image):
-//                                        // execute on main queue
-//                                        OperationQueue.main.addOperation {
-//                                            self.currentWeatherIcon.image = image
-//                                        }
-//                                    case let .Failure(error):
-//                                        print("error downloading image: \(error)")
-//                                    }
-//                            })
-//            case let .Failure(error):
-//                print("Error fetching weather forecast: \(error)")
-//            }
-//        }
-        
     }
+    
+    // MARKS:- methods
     
     @objc func receivedForecastWeather(notification: Notification) {
         if notification.name.rawValue == "ForecastWeather Available" {
             let forecastWeatherList: WeatherForecastAPI = notification.userInfo!["forecastWeather"] as! WeatherForecastAPI
             
-            // fetch weather icons per weather object
-            self.cellViewModels = forecastWeatherList.list.map {
+            // retrieve the 5 day forecast from the posted weather forecast dictionary
+            let fiveDayForecast: [ForecastHour] = computeFiveDayForecast(from: forecastWeatherList.list)
+            
+            // fetch weather icons for a 5 day forecast and display data
+            self.cellViewModels = fiveDayForecast.map {
                 ForecastWeatherCell(weekDay: $0.dt_txt, minimumTemperature: $0.main.minimumTemperature, maximumTemperature: $0.main.maximumTemperature, iconURL: URL(string: "https://openweathermap.org/img/w/\($0.weather[0].icon).png")!)
             }
+            
             OperationQueue.main.addOperation {
                 self.forecastTableView.reloadData()
             }
-//
-//            self.store.fetchWeatherIconForWeather(weather: forecastWeatherList, completion: { (imageResult) in
-//                switch imageResult {
-//                case let .Success(image):
-//                    // execute on main queue
-//                    OperationQueue.main.addOperation {
-//                        self.currentWeatherIcon.image = image
-//                    }
-//                case let .Failure(error):
-//                    print("error downloading image: \(error)")
-//                }
-//            })
-
         }
+    }
+    
+    /*
+     * computeFiveDayForecast(from:) computes the next 5 days weather
+    */
+    
+    func computeFiveDayForecast(from weatherForecastList:[ForecastHour]) -> [ForecastHour] {
+        var dateTimes: [Int] = [] // an array of date times
+        var fiveDayForecast: [ForecastHour] = [] // an array to of ForecastHour objects
+        
+        // use ForecastHour's firstObject's DateTime to get the next days
+        var dateTimeInSecondsIncrement = weatherForecastList.first!.dt
+        
+        // generate date times
+        for _ in 0...4 {
+            // add first day
+            dateTimes.append(dateTimeInSecondsIncrement)
+            // find next DateTime
+            dateTimeInSecondsIncrement += 86400 // increment by 24 hours
+        }
+        
+        // generate five day weather forecast
+        for forecastDictionary in weatherForecastList {
+            for dateTime in dateTimes {
+                if forecastDictionary.dt == dateTime {
+                    // date time found, so add the associated weather dictionary
+                    fiveDayForecast.append(forecastDictionary)
+                }
+            }
+        }
+        
+        return fiveDayForecast
+    }
+    
+    func formatDate(dateString: String) -> String {
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        
+        guard let date = dateFormatter.date(from: dateString) else { return "" }
+        
+        let calendar = Calendar(identifier: .gregorian)
+        let weekDay = calendar.component(.weekday, from: date)
+        
+        var dayName: String = ""
+        
+        if let dayOfWeek = DaysOfTheWeek(rawValue: weekDay) {
+            print("day of week = \(dayOfWeek)")
+            switch dayOfWeek {
+            case .Monday:
+                dayName = "Monday"
+            case .Tuesday:
+                dayName = "Tuesday"
+            case .Wednesday:
+                dayName = "Wednesday"
+            case .Thursday:
+                dayName = "Thursday"
+            case .Friday:
+                dayName = "Friday"
+            case .Saturday:
+                dayName = "Saturday"
+            case .Sunday:
+                dayName = "Sunday"
+            }
+        }
+        
+        return dayName
     }
     
     // UITableViewDataSource
@@ -97,6 +135,13 @@ class WeatherForecastViewController: UIViewController, UITableViewDataSource, UI
         weatherCell.loadWeatherIcon(completion: { (image) in
             cell.conditionIcon.image = image
         })
+        
+        let today = " today"
+        if indexPath.row == 0 {
+            cell.weekDayLabel.text = "\(formatDate(dateString: weatherCell.weekDay))" + today
+        } else {
+            cell.weekDayLabel.text = "\(formatDate(dateString: weatherCell.weekDay))"
+        }
         
         cell.minTemperatureLabel.text = "\(weatherCell.minimumTemperature.rounded(.toNearestOrAwayFromZero))"
         cell.maxTemperatureLabel.text = "\(weatherCell.maximumTemperature.rounded(.toNearestOrAwayFromZero))"
